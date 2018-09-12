57ac6459ddf8b20545e9610f01344880a42302b1
stefan.vasic <stefan.vasic@NBG-ENG-019>
2018-09-12 09:45:21 +0200

    Added new entity classes need to be fixed

--- TripExpenses/src/com/software/engineering/spring/tripexspenses/domen/BusinessTrip.java
+++ TripExpenses/src/com/software/engineering/spring/tripexspenses/domen/BusinessTrip.java
@@ -1,163 +1,164 @@
-//package com.software.engineering.spring.tripexspenses.domen;
-//
-//import java.io.Serializable;
-//import java.util.Date;
-//import javax.persistence.Basic;
-//import javax.persistence.CascadeType;
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.Id;
-//import javax.persistence.JoinColumn;
-//import javax.persistence.ManyToOne;
-//import javax.persistence.NamedQueries;
-//import javax.persistence.NamedQuery;
-//import javax.persistence.OneToOne;
-//import javax.persistence.Table;
-//import javax.persistence.Temporal;
-//import javax.persistence.TemporalType;
-//import javax.validation.constraints.NotNull;
-//import javax.xml.bind.annotation.XmlRootElement;
-//
-//
-//@Entity
-//@Table(name = "BUSINESSTRIPS")
-//@XmlRootElement
-//@NamedQueries({
-//    @NamedQuery(name = "Businesstrips.findAll", query = "SELECT b FROM Businesstrips b")
-//    , @NamedQuery(name = "Businesstrips.findBybusTripId", query = "SELECT b FROM Businesstrips b WHERE b.busTripId = :busTripId")
-//    , @NamedQuery(name = "Businesstrips.findByFromdate", query = "SELECT b FROM Businesstrips b WHERE b.fromDate = :fromDate")
-//    , @NamedQuery(name = "Businesstrips.findByTodate", query = "SELECT b FROM Businesstrips b WHERE b.toDate = :toDate")
-//    , @NamedQuery(name = "Businesstrips.findByDayscount", query = "SELECT b FROM Businesstrips b WHERE b.daysCount = :daysCount")
-//    , @NamedQuery(name = "Businesstrips.findByLocationsLocid", query = "SELECT b FROM Businesstrips b WHERE b.locationLocId = :locationLocId")
-//    , @NamedQuery(name = "Businesstrips.findByTriptotalallowance", query = "SELECT b FROM Businesstrips b WHERE b.tripTotalAllowance = :tripTotalAllowance")})
-//public class BusinessTrip implements Serializable {
-//
-//    private static final long serialVersionUID = 1L;
-//    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
-//    @Id
-//    @Basic(optional = false)
-//    @NotNull
-//    @Column(name = "BUSTRIPID")
-//    private Long busTripId;
-//    @Column(name = "FROMDATE")
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date fromDate;
-//    @Column(name = "TODATE")
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date toDate;
-//    @Column(name = "DAYSCOUNT")
-//    private Long daysCount;
-//    @Basic(optional = false)
-//    @NotNull
-//    @Column(name = "LOCATIONS_LOCID")
-//    private Long locationLocId;
-//    @Column(name = "TRIPTOTALALLOWANCE")
-//    private Long tripTotalAllowance;
-//    @OneToOne(cascade = CascadeType.ALL, mappedBy = "businesstripsbusTripId")
-//    private TripBill tripBill;
-//    @JoinColumn(name = "EMPLOYEES_EMPLOYEEID", referencedColumnName = "EMPLOYEEID")
-//    @ManyToOne(optional = false)
-//    private Employee employeeEmployeeId;
-//
-//    public BusinessTrip() {
-//    }
-//
-//    public BusinessTrip(Long busTripId) {
-//        this.busTripId = busTripId;
-//    }
-//
-//    public BusinessTrip(Long busTripId, Long locationLocId) {
-//        this.busTripId = busTripId;
-//        this.locationLocId = locationLocId;
-//    }
-//
-//    public Long getBusTripId() {
-//        return busTripId;
-//    }
-//
-//    public void setBusTripId(Long busTripId) {
-//        this.busTripId = busTripId;
-//    }
-//
-//    public Date getFromDate() {
-//        return fromDate;
-//    }
-//
-//    public void setFromDate(Date fromDate) {
-//        this.fromDate = fromDate;
-//    }
-//
-//    public Date getToDate() {
-//        return toDate;
-//    }
-//
-//    public void setToDate(Date toDate) {
-//        this.toDate = toDate;
-//    }
-//
-//    public Long getDaysCount() {
-//        return daysCount;
-//    }
-//
-//    public void setDaysCount(Long daysCount) {
-//        this.daysCount = daysCount;
-//    }
-//
-//    public Long getLocationsLocId() {
-//        return locationLocId;
-//    }
-//
-//    public void setLocationsLocId(Long locationLocId) {
-//        this.locationLocId = locationLocId;
-//    }
-//
-//    public Long getTripTotalAllowance() {
-//        return tripTotalAllowance;
-//    }
-//
-//    public void setTripTotalAllowance(Long tripTotalAllowance) {
-//        this.tripTotalAllowance = tripTotalAllowance;
-//    }
-//
-//    public TripBill getTripBill() {
-//        return tripBill;
-//    }
-//
-//    public void setTripBill(TripBill tripBill) {
-//        this.tripBill = tripBill;
-//    }
-//
-//    public Employee getEmployeeEmployeeid() {
-//        return employeeEmployeeId;
-//    }
-//
-//    public void setEmployeeEmployeeid(Employee employeeEmployeeId) {
-//        this.employeeEmployeeId = employeeEmployeeId;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        int hash = 0;
-//        hash += (busTripId != null ? busTripId.hashCode() : 0);
-//        return hash;
-//    }
-//
-//    @Override
-//    public boolean equals(Object object) {
-//        // TODO: Warning - this method won't work in the case the id fields are not set
-//        if (!(object instanceof BusinessTrip)) {
-//            return false;
-//        }
-//        BusinessTrip other = (BusinessTrip) object;
-//        if ((this.busTripId == null && other.busTripId != null) || (this.busTripId != null && !this.busTripId.equals(other.busTripId))) {
-//            return false;
-//        }
-//        return true;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "domen.BusinessTrip[ busTripId=" + busTripId + " ]";
-//    }
-//    
-//}
+package com.software.engineering.spring.tripexspenses.domen;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author stefan.vasic
+ */
+@Entity
+@Table(name = "BUSINESSTRIPS")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Businesstrips.findAll", query = "SELECT b FROM Businesstrips b")
+    , @NamedQuery(name = "Businesstrips.findByBustripid", query = "SELECT b FROM Businesstrips b WHERE b.bustripid = :bustripid")
+    , @NamedQuery(name = "Businesstrips.findByFromdate", query = "SELECT b FROM Businesstrips b WHERE b.fromdate = :fromdate")
+    , @NamedQuery(name = "Businesstrips.findByTodate", query = "SELECT b FROM Businesstrips b WHERE b.todate = :todate")
+    , @NamedQuery(name = "Businesstrips.findByDayscount", query = "SELECT b FROM Businesstrips b WHERE b.dayscount = :dayscount")
+    , @NamedQuery(name = "Businesstrips.findByTriptotallow", query = "SELECT b FROM Businesstrips b WHERE b.triptotallow = :triptotallow")})
+public class Businesstrips implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "BUSTRIPID")
+    private BigDecimal bustripid;
+    @Column(name = "FROMDATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fromdate;
+    @Column(name = "TODATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date todate;
+    @Column(name = "DAYSCOUNT")
+    private BigInteger dayscount;
+    @Column(name = "TRIPTOTALLOW")
+    private BigInteger triptotallow;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "businesstripsBustripid")
+    private List<Tripbills> tripbillsList;
+    @JoinColumn(name = "EMPLOYEES_EMPLOYEEID", referencedColumnName = "EMPLOYEEID")
+    @ManyToOne(optional = false)
+    private Employees employeesEmployeeid;
+    @JoinColumn(name = "LOCATIONS_LOCID", referencedColumnName = "LOCID")
+    @ManyToOne(optional = false)
+    private Locations locationsLocid;
+
+    public Businesstrips() {
+    }
+
+    public Businesstrips(BigDecimal bustripid) {
+        this.bustripid = bustripid;
+    }
+
+    public BigDecimal getBustripid() {
+        return bustripid;
+    }
+
+    public void setBustripid(BigDecimal bustripid) {
+        this.bustripid = bustripid;
+    }
+
+    public Date getFromdate() {
+        return fromdate;
+    }
+
+    public void setFromdate(Date fromdate) {
+        this.fromdate = fromdate;
+    }
+
+    public Date getTodate() {
+        return todate;
+    }
+
+    public void setTodate(Date todate) {
+        this.todate = todate;
+    }
+
+    public BigInteger getDayscount() {
+        return dayscount;
+    }
+
+    public void setDayscount(BigInteger dayscount) {
+        this.dayscount = dayscount;
+    }
+
+    public BigInteger getTriptotallow() {
+        return triptotallow;
+    }
+
+    public void setTriptotallow(BigInteger triptotallow) {
+        this.triptotallow = triptotallow;
+    }
+
+    @XmlTransient
+    public List<Tripbills> getTripbillsList() {
+        return tripbillsList;
+    }
+
+    public void setTripbillsList(List<Tripbills> tripbillsList) {
+        this.tripbillsList = tripbillsList;
+    }
+
+    public Employees getEmployeesEmployeeid() {
+        return employeesEmployeeid;
+    }
+
+    public void setEmployeesEmployeeid(Employees employeesEmployeeid) {
+        this.employeesEmployeeid = employeesEmployeeid;
+    }
+
+    public Locations getLocationsLocid() {
+        return locationsLocid;
+    }
+
+    public void setLocationsLocid(Locations locationsLocid) {
+        this.locationsLocid = locationsLocid;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (bustripid != null ? bustripid.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Businesstrips)) {
+            return false;
+        }
+        Businesstrips other = (Businesstrips) object;
+        if ((this.bustripid == null && other.bustripid != null) || (this.bustripid != null && !this.bustripid.equals(other.bustripid))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "domen.Businesstrips[ bustripid=" + bustripid + " ]";
+    }
+    
+}
--- TripExpenses/src/com/software/engineering/spring/tripexspenses/domen/TripBill.java
+++ TripExpenses/src/com/software/engineering/spring/tripexspenses/domen/TripBill.java
@@ -1,113 +1,116 @@
-//package com.software.engineering.spring.tripexspenses.domen;
-//
-//import java.io.Serializable;
-//import java.util.List;
-//import javax.persistence.Basic;
-//import javax.persistence.CascadeType;
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.Id;
-//import javax.persistence.JoinColumn;
-//import javax.persistence.NamedQueries;
-//import javax.persistence.NamedQuery;
-//import javax.persistence.OneToMany;
-//import javax.persistence.OneToOne;
-//import javax.persistence.Table;
-//import javax.validation.constraints.NotNull;
-//import javax.xml.bind.annotation.XmlRootElement;
-//import javax.xml.bind.annotation.XmlTransient;
-//
-///**
-// *
-// * @author stefan.vasic
-// */
-//@Entity
-//@Table(name = "TRIPBILLS")
-//@XmlRootElement
-//@NamedQueries({
-//    @NamedQuery(name = "Tripbills.findAll", query = "SELECT t FROM Tripbills t")
-//    , @NamedQuery(name = "Tripbills.findByTripbillid", query = "SELECT t FROM Tripbills t WHERE t.tripBillId = :tripBillId")
-//    , @NamedQuery(name = "Tripbills.findByTotalamount", query = "SELECT t FROM Tripbills t WHERE t.totalAmount = :totalAmount")})
-//public class TripBill implements Serializable {
-//
-//    private static final long serialVersionUID = 1L;
-//    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
-//    @Id
-//    @Basic(optional = false)
-//    @NotNull
-//    @Column(name = "TRIPBILLID")
-//    private Long tripBillId;
-//    @Column(name = "TOTALAMOUNT")
-//    private Long totalAmount;
-////    @JoinColumn(name = "BUSINESSTRIPS_BUSTRIPID", referencedColumnName = "BUSTRIPID")
-////    @OneToOne(optional = false)
-////    private BusinessTrip businessTripBusTripId;
-//    @OneToMany(cascade = CascadeType.ALL, mappedBy = "tripBill")
-//    private List<Bill> billList;
-//
-//    public TripBill() {
-//    }
-//
-//    public TripBill(Long tripBillId) {
-//        this.tripBillId = tripBillId;
-//    }
-//
-//    public Long getTripBillId() {
-//        return tripBillId;
-//    }
-//
-//    public void setTripbillid(Long tripBillId) {
-//        this.tripBillId = tripBillId;
-//    }
-//
-//    public Long getTotalAmount() {
-//        return totalAmount;
-//    }
-//
-//    public void setTotalAmount(Long totalAmount) {
-//        this.totalAmount = totalAmount;
-//    }
-//
-////    public BusinessTrip getBusinessTripBustripId() {
-////        return businessTripBusTripId;
-////    }
-////
-////    public void setBusinessTripBustripid(BusinessTrip businessTripBusTripId) {
-////        this.businessTripBusTripId = businessTripBusTripId;
-////    }
-//
-//    @XmlTransient
-//    public List<Bill> getBillList() {
-//        return billList;
-//    }
-//
-//    public void setBillList(List<Bill> billList) {
-//        this.billList = billList;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        int hash = 0;
-//        hash += (tripBillId != null ? tripBillId.hashCode() : 0);
-//        return hash;
-//    }
-//
-//    @Override
-//    public boolean equals(Object object) {
-//        // TODO: Warning - this method won't work in the case the id fields are not set
-//        if (!(object instanceof TripBill)) {
-//            return false;
-//        }
-//        TripBill other = (TripBill) object;
-//        if ((this.tripBillId == null && other.tripBillId != null) || (this.tripBillId != null && !this.tripBillId.equals(other.tripBillId))) {
-//            return false;
-//        }
-//        return true;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "domen.TripBill[ tripBillId=" + tripBillId + " ]";
-//    }
-//    
-//}
+package com.software.engineering.spring.tripexspenses.domen;
+
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author stefan.vasic
+ */
+@Entity
+@Table(name = "TRIPBILLS")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Tripbills.findAll", query = "SELECT t FROM Tripbills t")
+    , @NamedQuery(name = "Tripbills.findByTripbillid", query = "SELECT t FROM Tripbills t WHERE t.tripbillid = :tripbillid")
+    , @NamedQuery(name = "Tripbills.findByTotalamount", query = "SELECT t FROM Tripbills t WHERE t.totalamount = :totalamount")})
+public class Tripbills implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "TRIPBILLID")
+    private BigDecimal tripbillid;
+    @Column(name = "TOTALAMOUNT")
+    private BigInteger totalamount;
+    @JoinColumn(name = "BUSINESSTRIPS_BUSTRIPID", referencedColumnName = "BUSTRIPID")
+    @ManyToOne(optional = false)
+    private Businesstrips businesstripsBustripid;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "tripbillsTripbillid")
+    private List<Bills> billsList;
+
+    public Tripbills() {
+    }
+
+    public Tripbills(BigDecimal tripbillid) {
+        this.tripbillid = tripbillid;
+    }
+
+    public BigDecimal getTripbillid() {
+        return tripbillid;
+    }
+
+    public void setTripbillid(BigDecimal tripbillid) {
+        this.tripbillid = tripbillid;
+    }
+
+    public BigInteger getTotalamount() {
+        return totalamount;
+    }
+
+    public void setTotalamount(BigInteger totalamount) {
+        this.totalamount = totalamount;
+    }
+
+    public Businesstrips getBusinesstripsBustripid() {
+        return businesstripsBustripid;
+    }
+
+    public void setBusinesstripsBustripid(Businesstrips businesstripsBustripid) {
+        this.businesstripsBustripid = businesstripsBustripid;
+    }
+
+    @XmlTransient
+    public List<Bills> getBillsList() {
+        return billsList;
+    }
+
+    public void setBillsList(List<Bills> billsList) {
+        this.billsList = billsList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (tripbillid != null ? tripbillid.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Tripbills)) {
+            return false;
+        }
+        Tripbills other = (Tripbills) object;
+        if ((this.tripbillid == null && other.tripbillid != null) || (this.tripbillid != null && !this.tripbillid.equals(other.tripbillid))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "domen.Tripbills[ tripbillid=" + tripbillid + " ]";
+    }
+    
+}
